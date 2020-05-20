ec42b53f4323fc3a1418c2995aa8a9143d4a8da4
n19l <jeanpaulngalula07@gmail.com>
2020-05-20 14:29:57 +0200

    wip add usermodel

--- web/Models/UserModel.cs
+++ web/Models/UserModel.cs
@@ -1,4 +1,11 @@
+using System;
+
 namespace web.Models
 {
-
+	public class UserModel
+	{
+		public int userId { get; set; }
+		public string email { get; set; }
+		public string password { get; set; }
+	}
 }
